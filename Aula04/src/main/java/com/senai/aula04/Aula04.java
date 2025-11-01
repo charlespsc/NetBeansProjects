e590db4b68b47efccc9c6815d15de583c2e14208
Prof. Charles Pereira <charlespereira.fln@gmail.com>
2025-11-01 19:55:59 -0300

    Aula04

---
+++ Aula04/src/main/java/com/senai/aula04/Aula04.java
@@ -0,0 +1,12 @@
+package com.senai.aula04;
+
+/**
+ *
+ * @author charles
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
