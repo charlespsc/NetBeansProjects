914388b0bbddc2d3421640057b85701114511d47
Prof. Charles Pereira <charlespereira.fln@gmail.com>
2025-11-01 20:18:06 -0300

    Aula04 update1

--- Aula04/src/main/java/com/senai/aula04/Aula04.java
+++ Aula04/src/main/java/com/senai/aula04/Aula04.java
@@ -7,6 +7,13 @@ package com.senai.aula04;
 public class Aula04 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Caneta c1 = new Caneta();
+        //metodo acessor e sem metodo acessor (ACESSO PUBLIC ou PRIVATE)
+        c1.setModelo("BIC");
+        //c1.modelo = "BIC";
+        
+        c1.setPonta(0.5f);
+        //c1.ponta = 0.5f;
+        c1.status();
     }
 }
---
+++ Aula04/src/main/java/com/senai/aula04/Caneta.java
@@ -0,0 +1,31 @@
+
+package com.senai.aula04;
+
+/**
+ *
+ * @author charles
+ */
+public class Caneta {
+    private String modelo;
+    private float ponta;
+    
+    public String getModelo(){
+        return this.modelo;
+    }
+    public void setModelo(String m){
+        this.modelo = m;
+    }
+    
+    public float getPonta(){
+        return this.ponta;
+    }
+    public void setPonta(float p){
+        this.ponta = p;
+    }
+    
+    public void status(){
+        System.out.println("SOBRE A CANETA:");
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Ponta: " + this.ponta);
+    }
+}
