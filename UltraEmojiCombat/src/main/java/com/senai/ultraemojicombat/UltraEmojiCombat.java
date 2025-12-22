5c9115b2d41be5451d0e98e795453e59263fe466
Prof. Charles Pereira <charlespereira.fln@gmail.com>
2025-12-22 17:15:24 -0300

    UltraEmojiCombat

---
+++ UltraEmojiCombat/src/main/java/com/senai/ultraemojicombat/UltraEmojiCombat.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.senai.ultraemojicombat;
+
+/**
+ *
+ * @author charles
+ */
+public class UltraEmojiCombat {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
