9aac05ec61033f8148cac96a39c46888d2c2bfa1
Prof. Charles Pereira <charlespereira.fln@gmail.com>
2026-01-09 22:08:26 -0300

    aula13-Polimorfismo2-final

--- Aula13/src/main/java/com/senai/aula13/Aula13.java
+++ Aula13/src/main/java/com/senai/aula13/Aula13.java
@@ -9,7 +9,15 @@ public class Aula13 {
 
     public static void main(String[] args) {
         //Programa Principal
-        Cachorro x = new Cachorro();
-        x.emitirSom();
+        Cachorro c = new Cachorro();
+        
+        c.reagir("Ola");
+        c.reagir("Vai apanhar!");
+        c.reagir(11,45);
+        c.reagir(19,00);
+        c.reagir(true);
+        c.reagir(false);
+        c.reagir(2, 12.5f);
+        c.reagir(17, 4.5f);
     }
 }
